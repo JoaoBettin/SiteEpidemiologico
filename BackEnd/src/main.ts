ec8ebc760f79bfba1e8180f1cdc3fd7126cb087f
JoaoBettin <jbettinsouza@gmail.com>
2026-04-30 00:13:39 -0300

    acesso a tudo

--- BackEnd/src/main.ts
+++ BackEnd/src/main.ts
@@ -4,9 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: 'https://front-end-production-bd8e.up.railway.app',
-});
+ app.enableCors();
   
   await app.listen(process.env.PORT ?? 3000);
   
